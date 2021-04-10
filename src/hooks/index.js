f6f7b75a2d1d2e489085c0b424cfc612e85f2bb8
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2021-04-10 18:07:40 -0400

    removed the content of hooks file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,20 +1,20 @@
-import * as cookie from 'cookie';
+// import * as cookie from 'cookie';
 
 
-export function getContext({ headers }) {
-    const cookies = cookie.parse(headers.cookie || '');
-    const jwt = cookies.jwt && Buffer.from(cookies.jwt, 'base64').toString('utf-8');
+// export function getContext({ headers }) {
+//     const cookies = cookie.parse(headers.cookie || '');
+//     const jwt = cookies.jwt && Buffer.from(cookies.jwt, 'base64').toString('utf-8');
 
-    return {
-        user: jwt ? JSON.parse(jwt) : null
-    };
-}
+//     return {
+//         user: jwt ? JSON.parse(jwt) : null
+//     };
+// }
 
-export function getSession({ context }) {
-    return {
-        user: context.user && {
-            fullname: context.user.fullname,
-            email: context.user.email
-        }
-    }
-}
+// export function getSession({ context }) {
+//     return {
+//         user: context.user && {
+//             fullname: context.user.fullname,
+//             email: context.user.email
+//         }
+//     }
+// }
